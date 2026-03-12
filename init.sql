2901d51eab8083710cab7b907be54a37744f5e09
Clifford Grimm <cgrimm01@users.noreply.github.com>
2026-03-12 15:18:42 -0400

    Modify database creation with encoding and locale

--- init.sql
+++ init.sql
@@ -2,6 +2,6 @@ CREATE ROLE mmuser LOGIN PASSWORD 'mmuser';
    
 IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mattermost')
 THEN
-  CREATE DATABASE mattermost OWNER mmuser;
-  GRANT ALL PRIVILEGES ON DATABASE mattermost TO mmuser;
+   CREATE DATABASE mattermost WITH ENCODING 'UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8' TEMPLATE=template0;
+   GRANT ALL PRIVILEGES ON DATABASE mattermost to mmuser;
 END IF;
